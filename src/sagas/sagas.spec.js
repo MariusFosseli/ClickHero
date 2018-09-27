cb83e8399a697f8f4e52754f8488b06a16e43c8c
Marius Fosseli <mariussimonsen.fosseli@skatteetaten.no>
2018-09-27 08:42:48 +0200

    fixed errer when teting sagas

--- src/sagas/sagas.spec.js
+++ src/sagas/sagas.spec.js
@@ -1,4 +1,5 @@
 import test from 'tape';
+
 import { put, call } from 'redux-saga/effects';
 import { delay } from 'redux-saga';
 
@@ -10,17 +11,17 @@ test('attackAsync click test', (assert) => {
   assert.deepEqual(
     gen.next().value,
     call(delay, 1000),
-  )
+  );
 
   assert.deepEqual(
-    put.next().value,
-    put({type: "ATTACK_ASYNC"}),
-  )
+    gen.next().value,
+    put({type: "CLICK_ATTACK_MONSTER"}),
+  );
 
   assert.deepEqual(
     gen.next(),
     { done: true, value: undefined }
-  )
+  );
 
   assert.end()
 });
